b253567b37a188ff59d518fd3bdb438fcce40a97
ahakits <ahakits1987@gmail.com>
2020-02-12 00:53:46 +0900

    Sample08 Set Values to Specified Object Shader

---
+++ Assets/Scripts/Sample09.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Sample
+{
+    public class Sample09 : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject targetA;
+
+        [SerializeField]
+        private GameObject targetB;
+
+        private Renderer rendererA;
+
+        private Renderer rendererB;
+
+        private MaterialPropertyBlock materialPropertyBlock;
+
+        void Start()
+        {
+            materialPropertyBlock = new MaterialPropertyBlock();
+            materialPropertyBlock.SetFloat("_FloatValue", Random.value);
+
+            var materials = FindObjectsOfType<Material>();
+            Debug.Log("MATERIAL_COUNT_IN_SCENE :" + materials.Length);
+
+            foreach (var material in materials)
+            {
+                Debug.Log(material.name);
+            }
+
+            rendererA = targetA.GetComponent<Renderer>();
+            rendererB = targetB.GetComponent<Renderer>();
+            rendererB.SetPropertyBlock(materialPropertyBlock);
+        }
+
+        void Update()
+        {
+            rendererA.material.SetFloat("_FloatValue", Random.value);
+        }
+    }
+}
---
+++ Assets/Shaders/sample09.shader
@@ -0,0 +1,47 @@
+﻿Shader "Sample/Sample09"
+{
+    Properties
+    {
+        _FloatValue ("Float Value", Range(0, 1)) = 1
+    }
+
+    SubShader
+    {
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv: TEXCOORD;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+                float2 uv: TEXCOORD;
+            };
+
+            float _FloatValue;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 color = fixed4(_FloatValue, _FloatValue, _FloatValue, 1);
+                return color;
+            }
+            ENDCG
+        }
+    }
+}
